9cd21a5353fd8b995a696bd0d087ef774889826d
Kevin Sajan <kevsaj@gmail.com>
2021-02-26 23:28:44 -0500

    routes specific

--- server.js
+++ server.js
@@ -53,6 +53,33 @@ app.post("/api/workouts",(req,res)=>{
     });
 });
 
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id,
+        {
+            $push: {
+                "exercises": req.body
+            }
+        },
+        {
+            new: true
+        }
+    ).then(dbUpdateWorkout => {
+        res.json(dbUpdateWorkout);
+    })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+app.get("/api/workouts/range",(req,res)=>{
+    db.Workout.find({})
+    .then(dbWorkouts =>{
+        res.json(dbWorkouts);
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
